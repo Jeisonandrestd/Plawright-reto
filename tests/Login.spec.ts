8512c860eff01828d0c714fc1836302aa9b8bf35
Jeisonandrestd <jeison.desarrollo@gmail.com>
2026-06-10 00:06:29 -0500

    Validaciones del login + Describe para optimizar

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -1,9 +1,29 @@
 import { expect, test } from '@playwright/test'
+//En el test.describe se le indica que para cada test debe hacer el goto(HRM PAGE) primero
+test.describe("Login to HRM", () => { 
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://opensource-demo.orangehrmlive.com/')
+    })
 
-test('Login to hrm', async ({ page }) => {
-    await page.goto('https://opensource-demo.orangehrmlive.com/')
-    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
-    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
-    await page.getByRole('button', { name: 'Login' }).click()
-    await expect(page.getByRole('link',{name:'Admin'})).toBeVisible()
+    test('Succesful login', async ({ page }) => {
+        await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+        await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+        await page.getByRole('button', { name: 'Login' }).click()
+        //Comprobar componente en el dashboard para asegurar que ingresó correctamente:
+        await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+    })
+
+    test('invalid credentials alert', async ({ page }) => {
+        await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+        await page.getByRole('textbox', { name: 'Password' }).fill('sol123')
+        await page.getByRole('button', { name: 'Login' }).click()
+        await expect(page.getByRole('alert')).toContainText('Invalid credentials')
+    })
+
+    test('required fields login', async ({ page }) => {
+        await page.getByRole('button', { name: 'Login' }).click()
+        //Al dar click sin completar los campos, deben aparecer las alertas de campo requerido (2: username y password)
+        await expect(page.getByText('Required')).toHaveCount(2)
+    })
 })
+
