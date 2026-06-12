448eb12ce5ae141b4e535bd313ec560f69caa216
Jeisonandrestd <jeison.desarrollo@gmail.com>
2026-06-11 21:28:12 -0500

    Reto 2: Updated

--- tests/User.spec.ts
+++ tests/User.spec.ts
@@ -1,32 +1,57 @@
 import { test, expect } from '@playwright/test'
 
-test('Get all the usernames registered', async ({ page }) => {
-    await page.goto('https://opensource-demo.orangehrmlive.com/')
-    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
-    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
-    await page.getByRole('button', { name: 'Login' }).click()
-    //Comprobar componente en el dashboard para asegurar que ingresó correctamente:
-    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+test.describe("Login to HRM", () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://opensource-demo.orangehrmlive.com/')
+        await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+        await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+        await page.getByRole('button', { name: 'Login' }).click()
+        //Comprobar componente en el dashboard para asegurar que ingresó correctamente:
+        await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+    })
 
-    await page.getByRole('link', { name: 'Admin' }).click()
-    //Buscar User Management en el menú
-    await page.getByRole('navigation',{name: 'Topbar menu'}).getByText('User Management').click()
-    //Click en la opción del menú
-    await page.getByRole('menuitem', {name: 'Users'}).click()
-    //Detectar la tabla y tomar las filas
-    const rows = page.getByRole('table').getByRole('row')
-    const usernames: string [] = []
-    const rowCount = await rows.count()
-
-    for (let i=1; i< rowCount; i++){
-        const cell = rows.nth(i).getByRole('cell').nth(1) 
-        const username = await cell.textContent()
-        if (username){
-             usernames.push(username)
+    test('Get all the usernames registered', async ({ page }) => {
+        await page.getByRole('link', { name: 'Admin' }).click()
+        //Buscar User Management en el menú
+        await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('User Management').click()
+        //Click en la opción user del menú
+        await page.getByRole('menuitem', { name: 'Users' }).click()
+        //Detectar la tabla y tomar las filas
+        const rows = page.getByRole('table').getByRole('row') //Identifica la tabla y las filas
+        const usernames: string[] = [] //Establece arreglo 
+        const rowCount = await rows.count() //cuenta cuantas filas son
+        //Ciclo for para recorrer las filas: primera fila - titulos
+        for (let i = 1; i < rowCount; i++) {
+            const cell = rows.nth(i).getByRole('cell').nth(1) //nth para posiciones
+            const username = await cell.textContent() //Para obtener el valor
+            if (username) {
+                usernames.push(username)//Para agregar valor al arreglo
+            }
         }
-       
-    }
-    console.log(usernames)
+        console.log('Cantidad de registros:', rowCount - 1)
+        console.log('Usernames:', usernames)
+    })
 
+    test('Get all the employee names registered', async ({ page }) => {
+        await page.getByRole('link', { name: 'Admin' }).click()
+        //Buscar User Management en el menú
+        await page.getByRole('navigation', { name: 'Topbar menu' }).getByText('User Management').click()
+        //Click en la opción user del menú
+        await page.getByRole('menuitem', { name: 'Users' }).click()
+        //Detectar la tabla y tomar las filas
+        const rows = page.getByRole('table').getByRole('row') //Identifica la tabla y las filas
+        const employeeNames: string[] = [] //Establece arreglo 
+        const rowCount = await rows.count() //cuenta cuantas filas son
+        //Ciclo for para recorrer las filas: primera fila - titulos
+        for (let i = 1; i < rowCount; i++) {
+            const cell = rows.nth(i).getByRole('cell').nth(3) //nth para posiciones
+            const employee = await cell.textContent() //Para obtener el valor
+            if (employee) {
+                employeeNames.push(employee)//Para agregar valor al arreglo
+            }
+        }
+        console.log('Cantidad de registros:', rowCount - 1)
+        console.log('Employee Names:', employeeNames)
+    })
 
 })
